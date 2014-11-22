a4b83412f6605695a35abfde8b342d2b251e2fcc
EdoPut <edoardo.putti@gmail.com>
2014-11-22 14:17:52 +0100

    removed routing from app

--- app/js/app.js
+++ app/js/app.js
@@ -1,38 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
   (function ($) {
-    var bus = new BusModel();
-
-    searchPresenter({
-      el: 'section#home',
-      model: bus,
-      nextRoute: '#list'
-    });
-    listPresenter({
-      el: 'section#list',
-      model: bus,
-      nextRoute: 'details'
-    });
-    detailsPresenter({
-      el: 'section#details',
-      model: bus,
-      previousRoute: 'list'
-    });
-
-    $('body').on('click', 'a', function (e) {
-      e.preventDefault();
-      var link = $(this);
-      riot.route(link.attr('href'));
-      console.log(link.attr('href'));
-    });
-
-    riot.route(function (path) {
-      var a = document.querySelectorAll('section[role=region]');
-      for (var i = a.length - 1; i >= 0; i--) {
-        a[i].hidden = true;
+    var textValue = document.getElementById('searchText');
+    
+    $('form').on('submit', function (e) {
+      if(!textValue.value){
+        e.preventDefault();
       }
-      document.querySelector('section' + path).hidden = false;
+      window.localStorage.setItem('searchText', textValue.value);
     });
-
   })(jQuery);
 });
